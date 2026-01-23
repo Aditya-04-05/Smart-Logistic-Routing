10380e3e0dcb6f15ef888ea94f8e0fd674856353
Aditya Garg <gargaditya59@gmail.com>
2026-01-23 16:57:28 +0530

    added driverroutes

--- logistic-backend/src/app.js
+++ logistic-backend/src/app.js
@@ -3,11 +3,13 @@ const express = require("express");
 require("./config/db");
 const vehicleRoutes = require("./routes/vehicle.routes");
 const orderRoutes = require("./routes/order.routes");
+const driverRutes = require("./routes/driver.routes");
 const assignmentRoutes = require("./routes/assignment.routes");
 const app = express();
 
 app.use(express.json());
 app.use("/api/vehicles", vehicleRoutes);
+app.use("api/drivers", driverRutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/assignments", assignmentRoutes);
 app.get("/", (req, res) => {
